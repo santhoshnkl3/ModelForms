89514b7807c302c31bdf484fea4dacb2a142aa50
SANTHOSHKUMAR THANGAVEL <santhoshnkl3@gmail.com>
2019-02-18 22:33:42 +0530

    Secondcommit

--- viewcreation/forms.py
+++ viewcreation/forms.py
@@ -3,8 +3,8 @@ from django.forms import CharField,IntegerField,DateField
 
 
 class Add_Vehicle(forms.Form):
-    vehicle_number=forms.CharField(max_length=10,label='exampleInputPassword1')
-    vehicle_chasis_number=forms.CharField(max_length=32,label='exampleInputPassword1')
-    vehicle_engine_number=forms.CharField(max_length=32,label='exampleInputPassword1')
-    vehicle_insurance_dt=forms.DateField(label='exampleInputPassword1')
-    vehicle_fc_dt=forms.DateField(label='exampleInputPassword1')
+    vehicle_number=forms.CharField(max_length=10)
+    vehicle_chasis_number=forms.CharField(max_length=32)
+    vehicle_engine_number=forms.CharField(max_length=32)
+    vehicle_insurance_dt=forms.DateField()
+    vehicle_fc_dt=forms.DateField()
